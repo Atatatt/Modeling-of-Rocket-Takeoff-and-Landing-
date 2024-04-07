d16433394437db58c46f892eebedae7f9fb57743
Atatatt <93887088+Atatatt@users.noreply.github.com>
2024-04-07 05:24:44 +0300

    Add files via upload

---
+++ out11111.m
@@ -0,0 +1,27 @@
+
+m_dot=132;
+burn_time = 162.25;
+
+m_pay=5000;
+m_dry=1360.7;
+m_zwf= m_pay + m_dry;
+
+v_0= 0.01;
+g=9.81;
+gam_0=pi/2;
+R_E = 6371e3;
+h_0=0;
+x_0=0;
+
+t_turn=30.0;
+gam_in=0.10;
+
+ 
+
+%plot(out.height)
+%plot(out.flightpat)
+%plot(out.pitch_angle)
+%plot(out.position_x_axis)
+%plot(out.speed)
+%plot(out.theta)
+
